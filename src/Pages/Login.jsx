b588c77e71906a68cf260396b1ebc16b2422752c
techmannih <manishchaudharyttt@gmail.com>
2023-12-23 16:54:49 +0530

    fix login and signup functionality

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,99 +1,124 @@
-import React, { useState } from 'react'
-import { useNavigate } from "react-router-dom";
+import React, { useState } from "react";
 
-
+import { Link } from "react-router-dom";
 const Login = () => {
-  const navigate = useNavigate();
+  const [loginData, setLoginData] = useState({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState(null);
 
-  const [emailid, setEmailid] = useState('')
-  const [password, setPassword] = useState('')
+  const handleChange = (e) => {
+    setLoginData({ ...loginData, [e.target.name]: e.target.value });
+  };
 
+  const handleLogin = async (e) => {
+    e.preventDefault();
 
-  async function handleSignup(e) {
-    e.preventDefault()
-
-
-    const req = {
-      email: emailid,
-      password: password
-    }
-  //  
     try {
-      const response = await fetch(`${import.meta.env.REACT_APP_API_URL}/login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(req),
-      });
-      const data = await response.json();
-      console.log(data)
-      if (response.ok) {
-        document.cookie = "userToken=" + data.success;
-        navigate('/home')
-        console.log("1")
-      } else{
-        alert("Please  Check Your Email and Password ")
-      }console.log("2")
-    } catch (error) {
-      console.error('Error occurred while authenticating', error);
-      console.log("3")
-    }
-  }
+      // Add logic to send the login data to your backend for authentication
+      console.log("Login data submitted:", loginData);
 
+      // Make an API call to your server to handle user authentication
+      const response = await fetch("http://localhost:8888/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(loginData),
+      });
+
+      if (response.ok) {
+        // Authentication successful, you can redirect or perform other actions
+        console.log("User authenticated successfully!");
+      } else {
+        // Authentication failed, handle errors
+        const responseBody = await response.json();
+        if (
+          responseBody.errors &&
+          (responseBody.errors.email ||
+            responseBody.errors.password === "Invalid credentials")
+        ) {
+          // Display an error message to the user indicating invalid credentials
+          setError("Invalid email or password. Please try again.");
+        } else {
+          console.error("Authentication failed:", response.statusText);
+        }
+      }
+    } catch (error) {
+      console.error("Error during authentication:", error.message);
+    }
+  };
 
   return (
-    
-    <div className=" h-screen flex items-center justify-center">
-    <div className="container-login bg-black flex  p-8 m-8 text-white border-white border-2  rounded-xl">
-      <div className="box ">
-        <div className="head">
-          <span className=''></span>
-          <div className='text-center py-6 text-xl font-bold '>Log In</div>
-          <span className=''></span>
-       
-        <div className="content">
-          <div className='form'>
-            <div className="m-5">
-              <input
-              className='bg-black p-2 hover:border-white hover:border-2  hover:rounded-xl'
-                type="email"
-                value={emailid}
-                onChange={(e) => setEmailid(e.target.value)}
-                name='email'
-                placeholder='Email ID'
-                spellCheck="false"
-                autoComplete='true' required />
-            </div>
-            <div className="m-5 ">
-              <input
-              className='bg-black p-2 hover:border-white hover:border-2  hover:rounded-xl'
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                name='password'
-                placeholder='Password' required />
-            </div>
-            <div className=" cursor-pointer text-center py-3 m-5 hover:bg-white hover:text-black  border-white border-2 rounded-2 rounded-xl" >
-              <input
-             className='cursor-pointer font-bold  '
-                type="submit"
-                value={"Login"}
-                onClick={handleSignup} />
-            </div>
-            <div className="flex m-5 justify-between font-bold ">
-              <input className='cursor-pointer' type="button" value="Sign Up" onClick={() => navigate('/signup')} />
-            
-              <input className='cursor-pointer submit' type="submit" value={"Need Help ?"} onClick={() => navigate('/forgot')} />
+    <div className="h-screen flex items-center justify-center">
+      <div className="container-login bg-black flex p-8 m-8 text-white border-white border-2 rounded-xl">
+        <div className="box">
+          <div className="head">
+            <span className=""></span>
+            <div className="text-center py-6 text-xl font-bold">Log In</div>
+            <span className=""></span>
+          </div>
+          <div className="content">
+            <form onSubmit={handleLogin} className="form">
+              <div className="m-5">
+                <input
+                  className="bg-black p-2 hover:border-white hover:border-2  hover:rounded-xl"
+                  type="email"
+                  value={loginData.email}
+                  onChange={handleChange}
+                  name="email"
+                  placeholder="Email ID"
+                  spellCheck="false"
+                  autoComplete="true"
+                  required
+                />
+              </div>
+              <div className="m-5 ">
+                <input
+                  className="bg-black p-2 hover:border-white hover:border-2  hover:rounded-xl"
+                  type="password"
+                  value={loginData.password}
+                  onChange={handleChange}
+                  name="password"
+                  placeholder="Password"
+                  required
+                />
+              </div>
+              <p className="text-red-500">{error}</p>
+              <div className="cursor-pointer text-center py-3 m-5 hover:bg-white hover:text-black  border-white border-2 rounded-2 rounded-xl">
+                <button
+                  type="submit"
+                  className="submit cursor-pointer font-bold"
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+            <div className="flex m-5 justify-between font-bold">
+              <Link to="/signup">
+                {" "}
+                <input
+                  className="cursor-pointer"
+                  type="button"
+                  value="Sign Up"
+                />
+              </Link>
+
+              <Link to="/forgot">
+                {" "}
+                <input
+                  className="cursor-pointer submit"
+                  type="button"
+                  value={"Need Help ?"}
+                />
+              </Link>
             </div>
           </div>
         </div>
       </div>
     </div>
-    </div>
-    </div>
-            
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,110 +1,119 @@
-import React, { useRef, useState } from 'react'
-import {useNavigate} from 'react-router-dom'
-// import dotenv from "dotenv";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
-  const navigate = useNavigate()
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
 
-  const [uservalue, setUservalue] = useState('')
-  const [emailvalue, setEmailvalue] = useState('')
-  const [passwordvalue, setPasswordvalue] = useState('')
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  async function handleSignup(e) {
-    e.preventDefault()
-
-    const req = {
-      "name": uservalue,
-      "email": emailvalue,
-      "password": passwordvalue
-    }
-    
     try {
-      const response = await fetch(`${import.meta.env.REACT_APP_API_URL}/signup`, {
-        method: 'POST',
+      // Add logic to send the form data to your backend for user registration
+      console.log("Form data submitted:", formData);
+
+      // Make an API call to your server to handle user registration
+      const response = await fetch("http://localhost:8888/signup", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(req),
+        body: JSON.stringify(formData),
       });
-    
+
       if (response.ok) {
-        navigate('/')
-        console.log("ee")
+        // Registration successful, you can redirect or perform other actions
+        console.log("User registered successfully!");
+        console.log(formData)
       } else {
-        // Handle signup error based on the response
-        // alert("error")
-
-        console.log("eeddd")
-
+        // Registration failed, handle errors
+        console.error("Registration failed:", response.statusText);
       }
     } catch (error) {
-      console.error('Error occurred while making the signup request', error);
-      // alert("jjj")
-
-      console.log("eeddddddwwww")
+      console.error("Error during registration:", error.message);
     }
-   
-  }
-  
+  };
+
   return (
-    <div className=" h-screen flex items-center justify-center">
-    <div className="container-login bg-black flex  p-8 m-8 text-white border-white border-2 rounded-2 rounded-xl  ">
-      <div className="box">
-        <div className="head">
-          <span className=''></span>
-          <div className='text-center py-6 text-xl font-bold '>Sign Up</div>
-          <span className=''></span>
-        </div>
-        <div className="content">
-          <div className='form'>
-            <div className="m-5">
-              <input
-              className='bg-black p-2 hover:border-white hover:border-2  hover:rounded-xl'
-                type="text"
-                value={uservalue}
-                onChange={(e) => setUservalue(e.target.value) }
-                name='name'
-                placeholder='Full Name'
-                spellCheck="false"
-                autoComplete='false' required />
-            </div>
-            <div className="m-5">
-              <input className='bg-black p-2 hover:border-white hover:border-2  hover:rounded-xl'
-                type="email"
-                value={emailvalue}
-                onChange={(e) => setEmailvalue(e.target.value) }
-                name='email'
-                placeholder='Email ID'
-                spellCheck="false"
-                autoComplete='true' required />
-            </div>
-            <div className="m-5">
-              <input className='bg-black p-2 hover:border-white hover:border-2  hover:rounded-xl'
-                type="password"
-                value={passwordvalue}
-                onChange={(e) => setPasswordvalue(e.target.value) }
-                name='password'
-                placeholder='Password' required />
-            </div>
-            <div className="cursor-pointer text-center py-3 m-5 hover:bg-white hover:text-black  border-white border-2 rounded-2 rounded-xl">
-              <input
-              
-                type="submit"
-                value={"Sign Up"}
-                className='submit cursor-pointer font-bold' onClick={handleSignup} />
-            </div>
-            <div className="flex m-5 justify-between font-bold ">
-              
-              <input className='cursor-pointer' type="button" value="Login" onClick={() => navigate('/')} />
-               
-              <input className='cursor-pointer submit' type="submit" value={"Need Help ?"} onClick={() => navigate('/forgot')} />
+    <div className="h-screen flex items-center justify-center">
+      <div className="container-login bg-black flex p-8 m-8 text-white border-white border-2 rounded-2 rounded-xl">
+        <div className="box">
+          <div className="head">
+            <span className=""></span>
+            <div className="text-center py-6 text-xl font-bold">Sign Up</div>
+            <span className=""></span>
+          </div>
+          <div className="content">
+            <form onSubmit={handleSubmit} className="form">
+              <div className="m-5">
+                <input
+                  type="text"
+                  name="fullName"
+                  value={formData.fullName}
+                  onChange={handleChange}
+                  placeholder="Full Name"
+                  className="bg-black p-2 hover:border-white hover:border-2  hover:rounded-xl"
+                  required
+                />
+              </div>
+              <div className="m-5">
+                <input
+                  type="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  placeholder="Email ID"
+                  className="bg-black p-2 hover:border-white hover:border-2  hover:rounded-xl"
+                  required
+                />
+              </div>
+              <div className="m-5">
+                <input
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  placeholder="Password"
+                  className="bg-black p-2 hover:border-white hover:border-2  hover:rounded-xl"
+                  required
+                />
+              </div>
+
+              <div className="cursor-pointer text-center py-3 m-5 hover:bg-white hover:text-black  border-white border-2 rounded-2 rounded-xl">
+                <button
+                  type="submit"
+                  className="submit cursor-pointer font-bold"
+                >
+                  Sign Up
+                </button>
+              </div>
+            </form>
+            <div className="flex m-5 justify-between font-bold">
+              <Link to="/">
+                {" "}
+                <button className="cursor-pointer" type="button">
+                  Login
+                </button>
+              </Link>
+              <Link to="/forgot">
+                {" "}
+                <button className="cursor-pointer submit" type="button">
+                  Need Help ?
+                </button>
+              </Link>
             </div>
           </div>
         </div>
       </div>
-    </div></div>
-  )
-}
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
